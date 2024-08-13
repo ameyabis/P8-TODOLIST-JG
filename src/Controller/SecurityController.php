db1174396d96d30d712eb5a1f8315be0e4600a35
JeremyGhes <j.ghesquiere@allianceautomotive.fr>
2024-08-13 12:01:46 +0200

    modification du login, pour faire fonctionner

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,12 +21,6 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route('login_check', name: 'login_check')]
-    public function loginCheck()
-    {
-        // This code is never executed.
-    }
-
     #[Route('/logout', name: 'app_logout')]
     public function logout()
     {
