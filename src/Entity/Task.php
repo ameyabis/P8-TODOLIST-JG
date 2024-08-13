7fd42209763d7ef2d3b87ecada3bc5332d6a31e8
JeremyGhes <j.ghesquiere@allianceautomotive.fr>
2024-08-13 15:53:04 +0200

    ajout de la relation OneToMany entre user et task

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -29,6 +29,9 @@ class Task
     #[ORM\Column]
     private bool $isDone;
 
+    #[ORM\ManyToOne(inversedBy: 'tasks')]
+    private ?User $user = null;
+
     public function __construct()
     {
         $this->createdAt = new \DateTime();
@@ -91,6 +94,18 @@ class Task
     public function toggle(bool $flag): static
     {
         $this->isDone = $flag;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
